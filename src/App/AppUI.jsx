f53d5d899a59caffc317fa4c84195782ababfef2
Andrés Álvarez <andres.alvarez094@hotmail.es>
2022-02-22 09:22:38 -0500

    fix: modify the logic to create a object for topic

--- src/App/AppUI.jsx
+++ src/App/AppUI.jsx
@@ -9,46 +9,57 @@ import CardForm from '../components/CardForm';
 import HeaderContainerButton from '../components/HeaderContainerButton';
 
 const initialData = {
-    id: null,
     topic: '',
     question: '',
-    answer: '',
+    answer: ''
+    // cards: [
+    //     { question: '', answer: '' }
+    // ]
 }
-
-const initialCard = {
-    question: 'Please, double click on me',
-    answer: 'Yeah!! In this way I work',
-}
-
-
 function AppUI() {
     const [openModal, setOpenModal] = useState(false)
-    const [dataCard, setDataCard] = useState(initialData)
-    const [dataBaseCards, setDataBaseCards] = useState([initialCard])
+    const [infoCard, setInfoCard] = useState(initialData)
+    const [dataBaseCards, setDataBaseCards] = useState([])
     const [dataBaseFilterCards, setDataBaseFilterCards] = useState([])
 
-    const handleClick = () => {
+    const handleClickModal = () => {
         setOpenModal(!openModal)
-        setDataCard(initialData)
+        setInfoCard(initialData)
     }
 
     const handleChange = (e) => {
-        setDataCard({
-            ...dataCard,
-            [e.target.name]: e.target.value
-        })
+        const newInfoCard = {
+            ...infoCard,
+            [e.target.name]: e.target.value,
+        }
+        setInfoCard(newInfoCard)
     }
 
     // CRUD
     const createCard = (card) => {
-        card.id = Date.now()
-        setDataBaseCards([
-            ...dataBaseCards,
-            {
-                ...card,
-            }
-        ])
-        setDataCard(initialData)
+        const existTopic = dataBaseCards.find(el => el.topic === card.topic)
+
+        if (existTopic) {
+            existTopic.cards.push({
+                question: card.question,
+                answer: card.answer
+            })
+        } else {
+            const newCard = [
+                ...dataBaseCards,
+                {
+                    topic: card.topic,
+                    cards: [
+                        {
+                            question: card.question,
+                            answer: card.answer
+                        }
+                    ]
+                }
+            ]
+            setDataBaseCards(newCard)
+        }
+        setInfoCard(initialData)
         setOpenModal(false)
     }
 
@@ -57,30 +68,32 @@ function AppUI() {
         setDataBaseCards(newListOfCards)
     }
 
-    const getCardByTopic = (topic) => {
-        const newListOfCards = dataBaseCards.filter(card => card.topic === topic)
-        setDataBaseFilterCards(newListOfCards)
-    }
+    // const getCardByTopic = (topic) => {
+    //     const newListOfCards = dataBaseCards.filter(card => card.topic === topic)
+    //     setDataBaseFilterCards(newListOfCards)
+    // }
 
     console.log(dataBaseCards);
     return (
         <main className='main'>
             <HeaderContainerButton>
-                <Button onClick={handleClick} >Add</Button>
+                <Button onClick={handleClickModal} >Add</Button>
             </HeaderContainerButton>
             <NavbarTopics>
-                <li onClick={''} >All</li>
-                {
+                {/* <li onClick={''} >All</li>*/}
+                {/* {
                     dataBaseCards.map(el => (
-                        <li key={el.id} onClick={() => getCardByTopic(el.topic)} > {el.topic}</li>
+                        <li key={el.topic} onClick={() => getCardByTopic(el.topic)} > {el.topic}</li>
                     ))
-                }
+                } */}
             </NavbarTopics>
             <SectionCards>
                 {
                     ((dataBaseFilterCards.length === 0 ?
                         dataBaseCards :
-                        dataBaseFilterCards).map(el => (<Card key={el.id} el={el} deleteCard={deleteCard} />))
+                        dataBaseFilterCards).map((el, index) => {
+                            return el.cards.map((card, index) => <Card key={index} el={card} deleteCard={deleteCard} />)
+                        })
                     )
                 }
             </SectionCards>
@@ -88,15 +101,14 @@ function AppUI() {
                 openModal && (
                     <Modal>
                         <CardForm
-                            handleClick={handleClick}
-                            dataCard={dataCard}
+                            handleClick={handleClickModal}
+                            infoCard={infoCard}
                             handleChange={handleChange}
                             createCard={createCard}
                         />
                     </Modal>
                 )
             }
-
         </main>
     );
 }
--- src/components/CardForm/index.jsx
+++ src/components/CardForm/index.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 import Button from '../Button'
 import './cardForm.css'
 
-function CardForm({handleClick, dataCard, handleChange, createCard}) {
+function CardForm({handleClick, infoCard, handleChange, createCard}) {
 
     return (
         <form className='modal'>
             <label className='modal-topic'>
                 <span>Topic :</span>
-                <input value={dataCard.topic} onChange={handleChange} type='text' name='topic'/>
+                <input value={infoCard.topic} onChange={handleChange} type='text' name='topic'/>
             </label>
             <label className='modal-textarea'>
-                <textarea value={dataCard.question} onChange={handleChange} cols={40} rows={4} name='question' placeholder='What is the question' />
+                <textarea value={infoCard.question} onChange={handleChange} cols={40} rows={4} name='question' placeholder='What is the question' />
             </label>
             <label className='modal-textarea'>
-                <textarea value={dataCard.answer} onChange={handleChange} cols={40} rows={4} name='answer' placeholder='What is the answer' />
+                <textarea value={infoCard.answer} onChange={handleChange} cols={40} rows={4} name='answer' placeholder='What is the answer' />
             </label>
             <label className='modal-buttons'>
-                <Button onClick={() => createCard(dataCard)} >Add</Button>
+                <Button onClick={() => createCard(infoCard)} >Add</Button>
                 <Button modo='secondary' onClick={handleClick}>Cancel</Button>
             </label>
         </form>
